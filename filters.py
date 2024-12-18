2d4963c61409c2163ad6bb3d7acb1346559a847d
Brett C. Dudo <brett@dudo.io>
2024-12-17 23:27:54 -0800

    _extensions

--- filters.py
+++ filters.py
@@ -4,3 +4,9 @@ def pluralize(word):
     """Pluralize a word using the inflect library."""
     engine = inflect.engine()
     return engine.plural(word)
+
+# A function to add filters to the Jinja2 environment
+def setup(env):
+    """Add custom filters to the Jinja2 environment."""
+    env.filters['pluralize'] = pluralize
+    return env
