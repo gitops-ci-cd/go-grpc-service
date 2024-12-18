6b2eed3d3cd9786048e12ef1bec10fd1e55e51af
Brett C. Dudo <brett@dudo.io>
2024-12-17 23:40:01 -0800

    fixes

--- filters.py
+++ filters.py
@@ -1,11 +1,14 @@
 import inflect
+from jinja2.ext import Extension
 
 def pluralize(word):
     """Pluralize a word using the inflect library."""
     engine = inflect.engine()
     return engine.plural(word)
 
-class CustomExtension:
+class CustomExtension(Extension):
+    """A custom Jinja2 extension to add filters."""
     def __init__(self, environment):
-        """Add custom filters to the Jinja2 environment."""
+        super().__init__(environment)
+        # Register the custom filter
         environment.filters['pluralize'] = pluralize
